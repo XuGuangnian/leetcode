0ff35e04dffd430c86c8d7bc5e4242ad633cebf4
XuGuangnian <gengen.wang@outlook.com>
2023-07-26 18:00:35 +0800

    Q98 solutions

---
+++ src/com/leetcode/editor/cn/Q98ValidateBinarySearchTree.java
@@ -0,0 +1,93 @@
+//给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。 
+//
+// 有效 二叉搜索树定义如下： 
+//
+// 
+// 节点的左子树只包含 小于 当前节点的数。 
+// 节点的右子树只包含 大于 当前节点的数。 
+// 所有左子树和右子树自身必须也是二叉搜索树。 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [2,1,3]
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [5,1,4,null,null,3,6]
+//输出：false
+//解释：根节点的值是 5 ，但是右子节点的值是 4 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围在[1, 10⁴] 内 
+// -2³¹ <= Node.val <= 2³¹ - 1 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 2093 👎 0
+
+
+package com.leetcode.editor.cn;
+
+import java.util.LinkedList;
+
+public class Q98ValidateBinarySearchTree {
+    public static void main(String[] args) {
+        Solution solution = new Q98ValidateBinarySearchTree().new Solution();
+        TreeNode root = new TreeNode(5,
+                new TreeNode(1, null, null),
+                new TreeNode(4,
+                        new TreeNode(3, null, null),
+                        new TreeNode(6, null, null)));
+        solution.isValidBST(root);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        TreeNode node = root;
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        long prev = Long.MIN_VALUE;
+        while (node != null || !stack.isEmpty()) {
+            if (node != null) {
+                stack.push(node);
+                node = node.left;
+            } else {
+                TreeNode pop = stack.pop();
+                if (prev >= pop.val) {
+                    return false;
+                }
+                prev = pop.val;
+                node = pop.right;
+            }
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
