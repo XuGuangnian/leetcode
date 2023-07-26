fef2d4cdf89f39b97c22cec58c1665a91d276da4
XuGuangnian <gengen.wang@outlook.com>
2023-07-26 18:34:40 +0800

    Q938 solutions

---
+++ src/com/leetcode/editor/cn/Q938RangeSumOfBst.java
@@ -0,0 +1,91 @@
+//给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [10,5,15,3,7,null,18], low = 7, high = 15
+//输出：32
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+//输出：23
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [1, 2 * 10⁴] 内 
+// 1 <= Node.val <= 10⁵ 
+// 1 <= low <= high <= 10⁵ 
+// 所有 Node.val 互不相同 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 328 👎 0
+
+
+package com.leetcode.editor.cn;
+
+public class Q938RangeSumOfBst {
+    public static void main(String[] args) {
+        Solution solution = new Q938RangeSumOfBst().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        // TreeNode node = root;
+        // LinkedList<TreeNode> stack = new LinkedList<>();
+        // int result = 0;
+        // while (node != null || !stack.isEmpty()) {
+        //     if (node != null) {
+        //         stack.push(node);
+        //         node = node.left;
+        //     } else {
+        //         TreeNode pop = stack.pop();
+        //         if (pop.val > high) {
+        //             break;
+        //         }
+        //         if (pop.val >= low) {
+        //             result += pop.val;
+        //         }
+        //         node = pop.right;
+        //     }
+        // }
+        // return result;
+
+        if (root == null) {
+            return 0;
+        }
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        } else if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        } else {
+            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
